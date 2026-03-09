ce70121a28c5feae2ac85eb478aef88be8b31602
Jitendra Yejare <11752425+jyejare@users.noreply.github.com>
2026-03-09 13:01:30 +0530

    Feast client config Auto Access to users and groups based on policies
    Signed-off-by: Jitendra Yejare <11752425+jyejare@users.noreply.github.com>

--- infra/feast-operator/internal/controller/services/services.go
+++ infra/feast-operator/internal/controller/services/services.go
@@ -141,9 +141,6 @@ func (feast *FeastServices) Deploy() error {
 	if err := feast.deployClient(); err != nil {
 		return err
 	}
-	if err := feast.deployNamespaceRegistry(); err != nil {
-		return err
-	}
 	if err := feast.deployCronJob(); err != nil {
 		return err
 	}
