83b8cf33c3d02ed38bc2f7c21aac469a7d051626
Vanshika Vanshika <vvanshik@redhat.com>
2026-09-22 16:02:36 +0530

    add missing rbac
    Signed-off-by: Vanshika Vanshika <vvanshik@redhat.com>
    
    rh-pre-commit.version: 2.3.2
    rh-pre-commit.check-secrets: ENABLED

--- infra/feast-operator/internal/controller/featurestore_controller.go
+++ infra/feast-operator/internal/controller/featurestore_controller.go
@@ -76,12 +76,12 @@ type FeatureStoreReconciler struct {
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=roles;rolebindings,verbs=get;list;create;update;watch;delete
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=get;list;create
 // +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterroles,verbs=update;delete,resourceNames=feast-data-registry-admin;feast-data-registry-editor;feast-data-registry-viewer;feast-discover-namespaces;feast-oidc-token-review;feast-token-review-cluster-role
-// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,verbs=create;get;list;update;delete
+// +kubebuilder:rbac:groups=rbac.authorization.k8s.io,resources=clusterrolebindings,verbs=create;get;list;watch;update;delete
 // namespaces update is required by access.EnsureNamespaceLabel and
 // RemoveNamespaceLabelIfLast, which write the opendatahub.io/feast
 // discovery label. Not present upstream; do not drop when syncing.
 // +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch,resourceNames=feast-capabilities-config
-// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;update
+// +kubebuilder:rbac:groups=core,resources=namespaces,verbs=get;list;watch;create;update
 // +kubebuilder:rbac:groups=core,resources=secrets,verbs=get
 // +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;watch;create;delete;deletecollection
 // +kubebuilder:rbac:groups=core,resources=pods/exec,verbs=create
